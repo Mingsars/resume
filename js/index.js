80fe1da6d8a23a6b037efa8ff37913ab87f93ca3
qiuqian <qiuqian@qiuqiandeiMac.local>
2018-10-10 16:49:08 +0800

    完善About Me

---
+++ js/index.js
@@ -0,0 +1,49 @@
+var About = $('#about'),
+    Skill = $('#skill'),
+    Experience = $('#experience'),
+    Production = $('#production'),
+    mainBlock = $('.mainBlock'),
+    mainLen = mainBlock.length;
+
+mainBlock.click(function(){
+    
+    $(this).css({   //点击之后，放大
+        width : '80%',
+        height : '100%',
+        top : 0,
+        left : 0,
+    },500);
+    $(this).find('.mainMsg').css('display','block');  //显示主要内容
+    $(this).find('.msg').css('display','none');     //隐藏简略信息
+    //针对性设置
+    if($(this).index() == 2){
+        console.log(1);
+        $(this).find('h1').css('fontSize','4rem');
+        $(this).find('h4').css('fontSize','3rem');
+        $(this).find('p').css({
+            'fontSize' : '1.5rem',
+            'marginBottom': '10px'
+        })
+    }
+
+
+   var arr = $(this).siblings('.mainBlock'),
+       index = 0;
+    arr.each(function(){    //点击一个，其他几个都缩小
+        var num = index * 35+ '%';
+        $(this).css({
+            width : '18%',
+            height : '30%',
+            top : num,
+            left : '82%',
+
+       })
+       $(this).find('.mainMsg').css('display','none');   //隐藏主要内容
+       $(this).find('.msg').css('display','block');    //显示简略信息
+       index++;
+       if(index > arr.length){
+           index = 0;
+       }
+    })
+
+})
