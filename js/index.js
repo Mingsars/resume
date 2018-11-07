cedc6ff0a8bcc0dfafd84605f4b4db1f55a92ec8
Mingsar <1162436458@qq.com>
2018-11-07 19:23:47 +0800

    完成初版

--- js/index.js
+++ js/index.js
@@ -3,10 +3,17 @@ var About = $('#about'),
     Experience = $('#experience'),
     Production = $('#production'),
     mainBlock = $('.mainBlock'),
-    mainLen = mainBlock.length;
+    mainLen = mainBlock.length,
+    timer = null,
+    flag = 99;
+
+//exp模块内容获取高度
+
 
 mainBlock.click(function(){
-    
+    if(flag == $(this).index()){
+        return;
+    }
     $(this).css({   //点击之后，放大
         width : '80%',
         height : '100%',
@@ -15,6 +22,8 @@ mainBlock.click(function(){
     },500);
     $(this).find('.mainMsg').css('display','block');  //显示主要内容
     $(this).find('.msg').css('display','none');     //隐藏简略信息
+
+
     //针对性设置
     if($(this).index() == 2){   //如果点击了about模块
         $(this).find('h1').css('fontSize','4rem');
@@ -26,9 +35,11 @@ mainBlock.click(function(){
         $(this).find('.flex-1 span').css('display','inline');
     }else if($(this).index() == 3){     //如果点击了skill模块
         $(this).find('.skill-item').css({'fontSize':'1.5rem',"lineHeight" : 2});
+    }else if($(this).index() == 4){      //如果点击了exp模块
+        
+    }else{      //如果点击了作品模块
+        $(this).find('a').css('display','inline');
     }
-
-
    var arr = $(this).siblings('.mainBlock'),
        index = 0;
     arr.each(function(){    //点击一个，其他几个都缩小
@@ -47,5 +58,4 @@ mainBlock.click(function(){
            index = 0;
        }
     })
-
 })
